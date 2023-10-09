5268d55dc89b5e7a988bc019b457a797bd974601
prasanna3228 <prasannakuma3228@gmail.com>
2023-10-10 04:41:16 +0530

    file set up

---
+++ index.js
@@ -0,0 +1,175 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+
+const dbPath = path.join(__dirname, "mydatabase.db");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3005, () => {
+      console.log("Server Running at http://localhost:3005/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Get Books API
+app.get("/details/", async (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401).send("invalid jwt token");
+  } else {
+    jwt.verify(jwtToken, "prasanna_kumar", async (error, user) => {
+      if (error) {
+        response.status(401).send("Invalid access token");
+      } else {
+        const getBooksQuery = `
+  SELECT
+    *
+  FROM
+    users
+  `;
+        const booksArray = await db.all(getBooksQuery);
+        response.send(booksArray);
+      }
+    });
+  }
+});
+
+// User Register API
+app.post("/insert/", async (req, res) => {
+  const user_name=req.body.user_name;
+  const user_email=req.body.user_email
+  const user_password=req.body.user_password
+  const user_image=req.body.user_image
+  const total_orders=req.body.total_orders
+  
+  const hashedPassword = await bcrypt.hash(user_password, 10);
+  const selectUserQuery = `
+    SELECT 
+      * 
+    FROM 
+      users
+    WHERE 
+      user_email = '${user_email}';`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+     INSERT INTO
+      users (user_name,user_email,user_password,user_image,total_orders) values (?,?,?,?,? );`;
+    await db.run(createUserQuery,[user_name,user_email,hashedPassword,user_image,total_orders]);
+    // res.send("User created successfully");
+    res.redirect("/")
+  } else {
+    res.status(400);
+    res.send("User already exists");
+  }
+});
+
+// User Login API
+app.post("/login/", async (request, response) => {
+  const { user_email, user_password } = request.body;
+  const selectUserQuery = `
+    SELECT
+      *
+    FROM
+      Users
+    WHERE 
+      user_email = '${user_email}';`;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(user_password, dbUser.user_password);
+    if (isPasswordMatched === true) {
+      const payload = { user_email: user_email };
+      const jwtToken = jwt.sign(payload, "prasanna_kumar");
+      response.send({ jwtToken });
+      //   response.send("Login Success!");
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+
+
+
+app.set("view engine", "ejs"); // Set EJS as the view engine
+app.set("views", path.join(__dirname, "views")); // Specify the views directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//get all user details without jwt
+app.get("/userdetails", async (request, response) => {
+  try {
+    const getUsersQuery = `
+      SELECT
+        *
+      FROM
+        users;`;
+
+    const users = await db.all(getUsersQuery);
+    response.render("table", { users }); // Render the "table.ejs" template and pass the users data
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/image/:user_id/", async (request, response) => {
+  try {
+    const { user_id } = request.params;
+    const getPlayerQuery = `
+      SELECT 
+        user_image 
+      FROM 
+        users 
+      WHERE 
+      user_id = ?`;
+    const image = await db.get(getPlayerQuery, [user_id]);
+    if (image) {
+      response.send(`
+      <div style="display:flex;flex-direction:row;justify-content:center;">
+      <div>
+      <img src="${image.user_image}" style="width: 250px;" alt="User Image" />
+      <div/>
+     
+      <div/>
+      `);
+    } else {
+      response.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.error("Error fetching user image:", error);
+    response.status(500).send("Internal Server Error");
+  }
+});
---
+++ sql.sql
@@ -0,0 +1,69 @@
+-- CREATE TABLE users (name text ,email text);
+
+-- UPDATE users SET name="00000" WHERE name="Bollu Prasanna Kumar"
+
+
+
+-- CREATE TABLE users (name text ,email text);
+
+
+
+
+-- INSERT INTO  users(name,email)
+-- VALUES("prasanna","prasanna@gmail.com")
+
+-- SELECT * FROM users
+
+-- DROP TABLE users
+
+-- CREATE TABLE users (name text ,email text);
+
+
+
+-- SELECT * FROM Users
+
+-- CREATE TABLE Users (
+--     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_name TEXT,
+--     user_email TEXT UNIQUE,
+--     user_password TEXT,
+--     user_image TEXT,
+--     total_orders INTEGER,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     last_logged_in TIMESTAMP
+-- );
+
+
+
+-- CREATE TABLE Users (
+--     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_name TEXT,
+--     user_email TEXT UNIQUE,
+--     user_password TEXT,
+--     user_image TEXT,
+--     total_orders INTEGER,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     last_logged_in TIMESTAMP
+-- );
+
+-- SELECT *FROM Users
+-- DROP TABLE Users
+
+
+
+
+-- INSERT INTO emp(user_name,user_email,user_password,user_image,total_orders)
+-- VALUES("kl","kl@gmail.com","1231231231","mypic",12)
+
+-- CREATE TABLE users (
+--     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_name TEXT,
+--     user_email TEXT UNIQUE,
+--     user_password TEXT,
+--     user_image TEXT,
+--     total_orders INTEGER,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     last_logged_in TIMESTAMP
+-- );
+
+SELECT * FROM users
